7790f962b21db17b803b8210639d17373958dc1b
Gudo Gieles <gbgieles@gmail.com>
2025-10-02 22:54:00 +0200

    Add recipe list with  cards and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import { RecipeListPage } from './pages/RecipeListPage';
+import { useState } from "react";
+import { RecipeListPage } from "./pages/RecipeListPage";
+//import RecipePage from "./components/ui/RecipePage";
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  return (
+    <div>
+      {selectedRecipe ? (
+        <RecipePage
+          ricpe={selectedRecipe}
+          onBack={() => setSelectedRecipe(null)}
+        />
+      ) : (
+        <RecipeListPage onSelectedRecipe={setSelectedRecipe} />
+      )}
+    </div>
+  );
 };
--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,13 +1,79 @@
-import { Center, Heading } from '@chakra-ui/react';
-import { data } from '../utils/data';
+import {
+  Box,
+  Image,
+  Text,
+  VStack,
+  Stack,
+  Badge,
+  Heading,
+} from "@chakra-ui/react";
+import { data } from "../utils/data";
 
-export const RecipeListPage = () => {
-  // You can play around with the console log, but ultimately remove it once you are done
-  console.log(data.hits[0].recipe.label);
-
+export const RecipeListPage = ({ onSelectedRecipe }) => {
   return (
-    <Center h="100vh" flexDir="column">
-      <Heading>Your Recipe App</Heading>
-    </Center>
+    <VStack spacing={6} p={6} align="stretch">
+      <Heading textAlign="center">Your Recipe App</Heading>
+      {data.hits.map((hit) => {
+        const recipe = hit.recipe;
+
+        return (
+          <Box
+            key={recipe.label}
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            p={4}
+            cursor="pointer"
+            onClick={() => onSelectedRecipe(recipe)}
+            _hover={{ bg: "gray.50" }}
+          >
+            <Stack direction="row" spacing={4}>
+              <Image
+                src={recipe.image}
+                alt={recipe.label}
+                boxSize="150px"
+                objectFit="cover"
+              />
+              <Box>
+                <heading size="md"> {recipe.label}</heading>
+
+                {recipe.dietLabels.length > 0 && (
+                  <Text>
+                    <strong>Diet</strong> {recipe.dietLabels.join(", ")}
+                  </Text>
+                )}
+                {recipe.cautions.length > 0 && (
+                  <Text>
+                    <strong>Warning</strong> {recipe.cautions.join(",")}
+                  </Text>
+                )}
+
+                {recipe.mealType && (
+                  <Text>
+                    <strong>Meal type</strong>
+                    {recipe.mealType.join(",")}
+                  </Text>
+                )}
+
+                {recipe.dishType && (
+                  <Text>
+                    <strong>Dish type:</strong> {recipe.dishType.join(", ")}
+                  </Text>
+                )}
+
+                <Stack direction="row" mt={2}>
+                  {recipe.healthLabels.includes("Vegan") && (
+                    <Badge colorScheme="green">Vegan</Badge>
+                  )}
+                  {recipe.healthLabels.includes("Vegetarian") && (
+                    <Badge colorScheme="purple">Vegetarian</Badge>
+                  )}
+                </Stack>
+              </Box>
+            </Stack>
+          </Box>
+        );
+      })}
+    </VStack>
   );
 };
